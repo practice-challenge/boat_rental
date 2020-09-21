717b9c04ce82a8a393c63b67764f8141fc115e16
Ryan Camp <campryan@comcast.net>
2020-09-21 12:00:34 -0600

    Write and pass attributes test for boat class

--- spec/boat_spec.rb
+++ spec/boat_spec.rb
@@ -2,7 +2,14 @@ RSpec.describe Boat do
   before do
     @kayak = Boat.new(:kayak, 20)
   end
+
   it "exists" do
     expect(@kayak).to be_an_instance_of(Boat)
+  end
+
+  it "has readable attributes" do
+    expect(@kayak.type).to be(:kayak)
+    expect(@kayak.price_per_hour).to eql(20)
+    expect(@kayak.hours_rented).to eql(0)
   end
 end
