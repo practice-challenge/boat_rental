e2fee42382b806d9a896ca8a5433aa49420a9dbc
Ryan Camp <campryan@comcast.net>
2020-09-21 12:21:28 -0600

    Write and pass attributes test in dock class

--- spec/dock_spec.rb
+++ spec/dock_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe Dock do
   it "exists" do
     expect(@dock).to be_an_instance_of(Dock)
   end
+
+  it "has readable attributes" do
+    expect(@dock.name).to eql('The Rowing Dock')
+    expect(@dock.max_rental_time).to eql(3)
+  end
 end
