e3ec79bdbf1d47e7a0f091167b94ebda7a864b5e
Ryan Camp <campryan@comcast.net>
2020-09-21 12:38:29 -0600

    Write test for charge method in dock class

--- spec/dock_spec.rb
+++ spec/dock_spec.rb
@@ -1,6 +1,11 @@
 RSpec.describe Dock do
   before do
     @dock = Dock.new("The Rowing Dock", 3)
+    @kayak_1 = Boat.new(:kayak, 20)
+    @kayak_2 = Boat.new(:kayak, 20)
+    @sup_1 = Boat.new(:standup_paddle_board, 15)
+    @patrick = Renter.new("Patrick Star", "4242424242424242")
+    @eugene = Renter.new("Eugene Crabs", "1313131313131313")
   end
 
   it "exists" do
@@ -14,28 +19,46 @@ RSpec.describe Dock do
   end
 
   it "can rent to a renter" do
-    kayak_1 = Boat.new(:kayak, 20)
-    kayak_2 = Boat.new(:kayak, 20)
-    sup_1 = Boat.new(:standup_paddle_board, 15)
-    patrick = Renter.new("Patrick Star", "4242424242424242")
-    eugene = Renter.new("Eugene Crabs", "1313131313131313")
-
     expect(@dock.rental_log).to eql({})
 
-    @dock.rent(kayak_1, patrick)
+    @dock.rent(@kayak_1, @patrick)
 
-    expect(@dock.rental_log).to eql({kayak_1 => patrick})
+    expect(@dock.rental_log).to eql({@kayak_1 => @patrick})
 
-    @dock.rent(kayak_2, patrick)
+    @dock.rent(@kayak_2, @patrick)
 
-    expect(@dock.rental_log).to eql({kayak_1 => patrick,
-                                     kayak_2 => patrick})
+    expect(@dock.rental_log).to eql({@kayak_1 => @patrick,
+                                     @kayak_2 => @patrick})
 
-    @dock.rent(sup_1, eugene)
+    @dock.rent(@sup_1, @eugene)
 
-    expect(@dock.rental_log).to eql({kayak_1 => patrick,
-                                     kayak_2 => patrick,
-                                     sup_1 => eugene})
+    expect(@dock.rental_log).to eql({@kayak_1 => @patrick,
+                                     @kayak_2 => @patrick,
+                                     @sup_1 => @eugene})
+  end
+
+  it "text" do
+    @dock.rent(@kayak_1, @patrick)
+    @dock.rent(@kayak_2, @patrick)
+    @dock.rent(@sup_1, @eugene)
+    @kayak_1.add_hour
+    @kayak_1.add_hour
+
+    expect(@dock.charge(@kayak_1)).to eql({
+      :card_number => "4242424242424242",
+      :amount => 40
+      })
+
+    @sup_1.add_hour
+    @sup_1.add_hour
+    @sup_1.add_hour
+    @sup_1.add_hour
+    @sup_1.add_hour
+
+    expect(@dock.charge(@sup_1)).to eql({
+      :card_number => "1313131313131313",
+      :amount => 45 
+      })
   end
 
 
