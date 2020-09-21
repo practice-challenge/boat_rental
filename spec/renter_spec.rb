390362c3e0d4847ec22d7ca25240b13444fe7292
Ryan Camp <campryan@comcast.net>
2020-09-21 12:09:07 -0600

    Write and pass attributes test in renter class

--- spec/renter_spec.rb
+++ spec/renter_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe Renter do
   it "exists" do
     expect(@renter).to be_an_instance_of(Renter)
   end
+
+  it "has readable attributes" do
+    expect(@renter.name).to eql('Patrick Star')
+    expect(@renter.credit_card_number).to eql('4242424242424242')
+  end
 end
